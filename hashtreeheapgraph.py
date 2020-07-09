549a6788d1b670997d2a91c614844dd5298fb041
varunrajrana <varunrajrana98@gmail.com>
2020-07-09 02:14:21 -0600

    heaps and graphs

---
+++ hashtreeheapgraph.py
@@ -0,0 +1,217 @@
+class HashMap:
+  def __init__(self, array_size):
+    self.array_size = array_size
+    self.array = [None for item in range(array_size)]
+
+  def hash(self, key, count_collisions=0):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code + count_collisions
+
+  def compressor(self, hash_code):
+    return hash_code % self.array_size
+
+  def assign(self, key, value):
+    array_index = self.compressor(self.hash(key))
+    current_array_value = self.array[array_index]
+
+    if current_array_value is None:
+      self.array[array_index] = [key, value]
+      return
+
+    if current_array_value[0] == key:
+      self.array[array_index] = [key, value]
+      return
+
+    # Collision!
+
+    number_collisions = 1
+
+    while(current_array_value[0] != key):
+      new_hash_code = self.hash(key, number_collisions)
+      new_array_index = self.compressor(new_hash_code)
+      current_array_value = self.array[new_array_index]
+
+      if current_array_value is None:
+        self.array[new_array_index] = [key, value]
+        return
+
+      if current_array_value[0] == key:
+        self.array[new_array_index] = [key, value]
+        return
+
+      number_collisions += 1
+
+    return
+
+  def retrieve(self, key):
+    array_index = self.compressor(self.hash(key))
+    possible_return_value = self.array[array_index]
+
+    if possible_return_value is None:
+      return None
+
+    if possible_return_value[0] == key:
+      return possible_return_value[1]
+
+    retrieval_collisions = 1
+
+    while (possible_return_value != key):
+      new_hash_code = self.hash(key, retrieval_collisions)
+      retrieving_array_index = self.compressor(new_hash_code)
+      possible_return_value = self.array[retrieving_array_index]
+
+      if possible_return_value is None:
+        return None
+
+      if possible_return_value[0] == key:
+        return possible_return_value[1]
+
+      retrieval_collisions += 1
+
+    return
+
+class TreeNode:
+  def __init__(self, value):
+    self.value = value # data
+    self.children = [] # references to other nodes
+
+  def add_child(self, child_node):
+    # creates parent-child relationship
+    print("Adding " + child_node.value)
+    self.children.append(child_node) 
+    
+  def remove_child(self, child_node):
+    # removes parent-child relationship
+    print("Removing " + child_node.value + " from " + self.value)
+    self.children = [child for child in self.children 
+                     if child is not child_node]
+
+  def traverse(self):
+    # moves through each node referenced from self downwards
+    nodes_to_visit = [self]
+    while len(nodes_to_visit) > 0:
+      current_node = nodes_to_visit.pop()
+      print(current_node.value)
+      nodes_to_visit += current_node.children
+
+class Vertex:
+  def __init__(self, value):
+    self.value = value
+    self.edges = {}
+
+  def add_edge(self, vertex, weight = 0):
+    self.edges[vertex] = weight
+
+  def get_edges(self):
+    return list(self.edges.keys())
+
+
+class Graph:
+  def __init__(self, directed = False):
+    self.graph_dict = {}
+    self.directed = directed
+
+  def add_vertex(self, vertex):
+    self.graph_dict[vertex.value] = vertex
+
+  def add_edge(self, from_vertex, to_vertex, weight = 0):
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    if not self.directed:
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    start = [start_vertex]
+    seen = {}
+    while len(start) > 0:
+      current_vertex = start.pop(0)
+      seen[current_vertex] = True
+      print("Visiting " + current_vertex)
+      if current_vertex == end_vertex:
+        return True
+      else:
+        vertices_to_visit = set(self.graph_dict[current_vertex].edges.keys())
+        start += [vertex for vertex in vertices_to_visit if vertex not in seen]
+    return False
+
+class MinHeap:
+  def __init__(self):
+    self.heap_list = [None]
+    self.count = 0
+
+  def parent_idx(self, idx):
+    return idx // 2
+
+  def left_child_idx(self, idx):
+    return idx * 2
+
+  def right_child_idx(self, idx):
+    return idx * 2 + 1
+
+  def child_present(self, idx):
+    return self.left_child_idx(idx) <= self.count
+  
+  def retrieve_min(self):
+    if self.count == 0:
+      print("No items in heap")
+      return None
+    
+    min = self.heap_list[1]
+    self.heap_list[1] = self.heap_list[self.count]
+    self.count -= 1
+    self.heap_list.pop()
+    self.heapify_down()
+    return min
+
+  def add(self, element):
+    self.count += 1
+    self.heap_list.append(element)
+    self.heapify_up()
+
+
+  def get_smaller_child_idx(self, idx):
+    if self.right_child_idx(idx) > self.count:
+      return self.left_child_idx(idx)
+    else:
+      left_child = self.heap_list[self.left_child_idx(idx)]
+      right_child = self.heap_list[self.right_child_idx(idx)]
+      if left_child < right_child:
+        return self.left_child_idx(idx)
+      else:
+        return self.right_child_idx(idx)
+    
+  def heapify_up(self):
+    idx = self.count
+    swap_count = 0
+    while self.parent_idx(idx) > 0:
+      if self.heap_list[self.parent_idx(idx)] > self.heap_list[idx]:
+        swap_count += 1
+        tmp = self.heap_list[self.parent_idx(idx)]
+        self.heap_list[self.parent_idx(idx)] = self.heap_list[idx]
+        self.heap_list[idx] = tmp
+      idx = self.parent_idx(idx)
+
+    element_count = len(self.heap_list)
+    if element_count > 10000:
+      print("Heap of {0} elements restored with {1} swaps"
+            .format(element_count, swap_count))
+      print("")    
+      
+  def heapify_down(self):
+    idx = 1
+    # starts at 1 because we swapped first and last elements
+    swap_count = 1
+    while self.child_present(idx):
+      smaller_child_idx = self.get_smaller_child_idx(idx)
+      if self.heap_list[idx] > self.heap_list[smaller_child_idx]:
+        swap_count += 1
+        tmp = self.heap_list[smaller_child_idx]
+        self.heap_list[smaller_child_idx] = self.heap_list[idx]
+        self.heap_list[idx] = tmp
+      idx = smaller_child_idx
+
+    element_count = len(self.heap_list)
+    if element_count >= 10000:
+      print("Heap of {0} elements restored with {1} swaps"
+            .format(element_count, swap_count))
+      print("")  
